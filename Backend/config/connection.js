c1f8ffeb0faf3bcf7752d9c043ee9422e0647ef4
sammybh6 <samyakbhargava9@gmail.com>
2022-11-04 18:37:47 +0530

    a2

--- Backend/config/connection.js
+++ Backend/config/connection.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "password",
+  user: "sammy",
+  password: "varunlund",
   database: "assetmanagementsystem",
   multipleStatements: true
 });
